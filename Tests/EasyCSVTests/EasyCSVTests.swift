7732886e34aea9b7154efca6ad19773d1b6853c6
Scott Andrew <scott@nwdm.tech>
2022-08-02 20:34:20 -0700

    Initial Commit

---
+++ Tests/EasyCSVTests/EasyCSVTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EasyCSV
+
+final class EasyCSVTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EasyCSV().text, "Hello, World!")
+    }
+}
